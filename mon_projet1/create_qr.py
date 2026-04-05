e2f1f31c0379e92e3d09a7a976cc07a290d45a7e
Swhjidou <elbousidiahmed@gmail.com>
2026-04-05 01:41:11 +0000

    QR code

---
+++ mon_projet1/create_qr.py
@@ -0,0 +1,21 @@
+import qrcode
+from PIL import Image
+
+qrphotos = "mon_projet1"
+url = f"https://github.com/Swhjidou/schoolqr.github.io/{qrphotos}"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("static/images/qr_code.png")
+
+print(f"QR code cree pour : {url}")
+print("Fichier sauvegarde : static/images/qr_code.png")
